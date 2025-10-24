b506948a99b3d60e86c293fcd4210ad10a11a691
saimore07 <moresai0073@gmail.com>
2025-10-24 10:00:54 +0530

    Add deployment configurations

--- app.py
+++ app.py
@@ -152,4 +152,9 @@ def privacy():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    
+    # Get port from environment variable (for production)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_ENV') != 'production'
+    
+    app.run(debug=debug, host='0.0.0.0', port=port)
